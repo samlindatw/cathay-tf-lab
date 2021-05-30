65130745a1ad673960a9c2e422d6c0ac1c6a3b9b
Sky Huang <samlinda@MarsMBP.local>
2021-05-30 15:07:58 +0800

    update

--- main.tf
+++ main.tf
@@ -2,10 +2,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_sns_topic" "cbl-toic-us-east" {
-  name = "cbl-topic-us-east"
-}
+# resource "aws_sns_topic" "cbl-toic-us-east" {
+#   name = "cbl-topic-us-east"
+# }
 
-resource "aws_sns_topic" "cbl-topic-u222" {
-  name = "cbl-topic-u222"
+# resource "aws_sns_topic" "cbl-topic-u222" {
+#   name = "cbl-topic-u222"
+# }
+
+module "ec2_with_mandatory_tagging" {
+    source = "github.com/samlindatw/hello-terraform-module"
+    Name = "CBL2-Server"
+    user_id = "samlinda"
+    EndDate = "next-week"
+
+ // Lab Task: Read the module code at https://github.com/samlindatw/hello-terraform-module
+ // to set the required inut parameter
+ // INSERT_ARGUMENT
+
 }
